6c0b4d2b6cd123fdf9c887e69b5cacbcd26003b4
Sravani M <sravani.manukonda7@gmail.com>
2019-03-05 22:19:53 +0530

    sslyzer full scan added

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ from flask import request
 from flask_cors import CORS
 from flask import jsonify
 
+import subprocess
 import json
 import random
 
@@ -13,8 +14,38 @@ CORS(app)
 json_decoder = json.JSONDecoder()
 json_encoder = json.JSONEncoder()
 
+
 class VulnerabilityScanTools:
-	def sslyzer_scan(self):
+	def cmd_rsp(self,cmd):
+		try:
+			rsp = subprocess.check_output(cmd)
+		except Exception as e:
+			print(e)
+			rsp = None
+		return rsp
+
+	def sslyzer_scan(self,domain,scan_type):
+		cmd = ["sslyze","--regular",domain]
+		op = self.cmd_rsp(cmd)
+		sslyzer_op = op.split("\n")
+		line_num = 0
+		start = 0
+		end = 0
+
+		if scan_type=="full":
+			for line in sslyzer_op:
+				if 'SCAN RESULTS' in line:
+					start = line_num
+
+				if 'SCAN COMPLETED' in line:
+					end = line_num
+			
+				line_num = line_num + 1
+
+			sslyzer_op = sslyzer_op[start+2:end]
+		
+		scan_op = "\n".join(sslyzer_op)
+		print(scan_op)
 		print("sslyzer")
 		return -1
 	
@@ -26,15 +57,14 @@ def pick_tool(tools_json):
 	status_message = 'Error'
 	return_data = None
 
-	
-	#tools_dict = json_encoder.encode({"name":"sslyzer","id":1})
+	tools_dict = json_encoder.encode({"name":"sslyzer","id":1})
 	tools_dict = json_decoder.decode(tools_dict)
 	tool_id = tools_dict["id"]
 	tool_name = tools_dict["name"] 
 	
 	if tool_name=="sslyzer":
 		ss = VulnerabilityScanTools()
-		ss.sslyzer_scan()
+		ss.sslyzer_scan("www.pes.edu:443","full")
 
 	return "success"
 
